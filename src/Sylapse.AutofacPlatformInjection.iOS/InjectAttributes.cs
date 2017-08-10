8a3b67d274184fab0ae535ad70d564e58a9911ce
Tom Pratt <tom.pratt@live.com>
2017-08-10 20:08:52 +0300

    adding ios lifecycle viewcontroller

---
+++ src/Sylapse.AutofacPlatformInjection.iOS/InjectAttributes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Sylapse.AutofacLateInjection;
+
+namespace Sylapse.AutofacPlatformInjection.iOS
+{
+	public class InjectViewDidLoad : InjectAttribute
+	{
+        public InjectViewDidLoad() : base(InjectionPoints.ViewDidLoad)
+		{
+		}
+	}
+}
---
+++ src/Sylapse.AutofacPlatformInjection.iOS/LifetimeViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Autofac;
+using UIKit;
+using Sylapse.AutofacLateInjection;
+
+namespace Sylapse.AutofacPlatformInjection.iOS
+{
+    public class LifetimeViewController : UIViewController
+	{
+		private ILifetimeScope _viewLifetimeScope;
+		private ILifetimeScope _viewInstanceScope;
+		private ILifetimeScope _viewLayoutScope;
+
+		public LifetimeViewController() : base()
+		{
+		}
+
+		public LifetimeViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+        public override void ViewDidLoad()
+        {
+			base.ViewDidLoad();
+
+			_viewLifetimeScope = AppRoot.Container.BeginViewLifetimeScope();
+			_viewInstanceScope = _viewLifetimeScope.BeginViewInstanceScope(builder =>
+			{
+				builder.RegisterInstance(this).As<UIViewController>().ExternallyOwned();
+			});
+			_viewLayoutScope = _viewInstanceScope.BeginViewLayoutScope();
+
+            _viewLayoutScope.Inject(this, InjectionPoints.ViewDidLoad);
+		}
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+            if (IsMovingFromParentViewController) {
+                _viewLayoutScope?.Dispose();
+                _viewInstanceScope?.Dispose();
+                _viewLifetimeScope?.Dispose();
+            }
+        }
+    }
+}
